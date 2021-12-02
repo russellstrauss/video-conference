40cd5100e8ca38830090410d0aef5e5a10b3bc20
Russell Strauss <russellstrauss@gmail.com>
2021-12-02 11:19:06 -0700

    https working

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const https = require('https');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// const server = require('http').createServer(app);
+
+const fs = require('fs')
+
+
+var privateKey = fs.readFileSync('localhost+2-key.pem');
+var certificate = fs.readFileSync('localhost+2.pem');
+const options = {
+	key: privateKey,
+	cert: certificate
+};
+const server = https.createServer(options, app).listen(443);
+
+
+
+
+
+
+const io = require('socket.io').listen(server);
 const { v4: uuidV4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
 	console.log('redirect');
@@ -27,11 +46,12 @@ io.on('connection', socket => {
 		
 		socket.join(roomId);
 		socket.to(roomId).broadcast.emit('user-connected', userId);
-
+		
 		socket.on('disconnect', () => {
 			socket.to(roomId).broadcast.emit('user-disconnected', userId);
 		});
 	});
 });
 
-server.listen(443);
+// https.createServer(options, app).listen(443);
+// server.listen(443);
